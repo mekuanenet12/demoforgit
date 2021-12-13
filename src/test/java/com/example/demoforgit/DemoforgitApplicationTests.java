565714321cbe18d2a8eb0e514623db4ea9f32989
MAntene1 <mekuanteneh@gmail.com>
2021-12-12 19:21:36 -0800

    Initial commit

---
+++ src/test/java/com/example/demoforgit/DemoforgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoforgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoforgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
